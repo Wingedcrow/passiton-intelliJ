327e5ae422fc30967133be8ff59764af7f397749
Brxdley69 <bradleybalramcts@gmail.com>
2026-03-31 20:57:58 -0400

    Update UserSession.java

--- src/main/java/com/cts/passiton/UserSession.java
+++ src/main/java/com/cts/passiton/UserSession.java
@@ -3,23 +3,23 @@ package com.cts.passiton;
 public class UserSession {
 
     private static int userId;
-    private static String email;
+    private static String password;
 
-    public static void startSession(int id, String userEmail) {
+    public static void startSession(int id, String userPass) {
         userId = id;
-        email = userEmail;
+        password = userPass;
     }
 
     public static int getUserId() {
         return userId;
     }
 
-    public static String getEmail() {
-        return email;
+    public static String getPass() {
+        return password;
     }
 
     public static void clearSession() {
         userId = 0;
-        email = null;
+        password = null;
     }
 }
