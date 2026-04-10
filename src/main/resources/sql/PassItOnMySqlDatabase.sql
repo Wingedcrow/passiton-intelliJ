0f7a4b356e67875ffb1119e3b1c59ab798896e6c
Joshua Howard <masterjoshuahoward@gmail.com>
2026-04-10 17:15:50 -0400

    5th and final push

--- src/main/resources/sql/PassItOnMySqlDatabase.sql
+++ src/main/resources/sql/PassItOnMySqlDatabase.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `passiton` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `passiton`;
 -- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: passiton
@@ -21,7 +23,7 @@ SET @@SESSION.SQL_LOG_BIN= 0;
 -- GTID state at the beginning of the backup 
 --
 
-SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '33818208-0c20-11f1-8e9c-dcfb4800e5a5:1-122';
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '33818208-0c20-11f1-8e9c-dcfb4800e5a5:1-166';
 
 --
 -- Table structure for table `deletedaccounts`
@@ -133,7 +135,7 @@ CREATE TABLE `tblrequest` (
   KEY `benefactor_id` (`benefactor_id`),
   CONSTRAINT `tblrequest_ibfk_1` FOREIGN KEY (`requester_id`) REFERENCES `tblusers` (`user_id`),
   CONSTRAINT `tblrequest_ibfk_2` FOREIGN KEY (`benefactor_id`) REFERENCES `tblusers` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -142,6 +144,7 @@ CREATE TABLE `tblrequest` (
 
 LOCK TABLES `tblrequest` WRITE;
 /*!40000 ALTER TABLE `tblrequest` DISABLE KEYS */;
+INSERT INTO `tblrequest` VALUES (4,1,'Fundamental of Hardware and Software','Books','HIGH','AGREED','School Cafeteria','12:00 to 12:15','2026-04-10 21:00:55','2026-04-10 21:06:07','2026-04-11 21:06:07',7,0,0),(5,7,'Goggles','Tools','LOW','CLAIMED',NULL,NULL,'2026-04-10 21:05:50','2026-04-10 21:13:29','2026-04-11 21:13:29',9,0,0),(6,3,'Understanding Information Technology','Books','LOW','OPEN',NULL,NULL,'2026-04-10 21:08:54',NULL,'2026-04-11 21:08:54',NULL,0,0),(7,3,'Microcontrollers','Computer Components','MEDIUM','CLAIMED','School Cafeteria','12:00 to 12:15','2026-04-10 21:08:59','2026-04-10 21:10:16','2026-04-11 21:10:16',7,0,0),(8,3,'How to Code','Books','HIGH','OPEN',NULL,NULL,'2026-04-10 21:09:03',NULL,'2026-04-11 21:09:03',NULL,0,0);
 /*!40000 ALTER TABLE `tblrequest` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -170,7 +173,7 @@ CREATE TABLE `tblusers` (
 
 LOCK TABLES `tblusers` WRITE;
 /*!40000 ALTER TABLE `tblusers` DISABLE KEYS */;
-INSERT INTO `tblusers` VALUES (1,'sallyleftfieldcts@gmail.com','sally','Leftfield','sally1'),(3,'joshuahowardcts@gmail.com','joshua','Howard','joshua1'),(7,'bradleybalramcts@gmail.com','Bradley','Balram','bradley1'),(9,'sarahcopper','sarah','Copper','sarah1');
+INSERT INTO `tblusers` VALUES (1,'sallyleftfieldcts@gmail.com','Sally','Leftfield','sally1'),(3,'joshuahowardcts@gmail.com','Joshua','Howard','howard1'),(7,'bradleybalramcts@gmail.com','Bradley','Balram','bradley1'),(9,'sarahcoppercts@gmail.com','Sarah','Copper','sarah1');
 /*!40000 ALTER TABLE `tblusers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -193,7 +196,7 @@ CREATE TABLE `tblusersupplies` (
   KEY `itemid` (`itemid`),
   CONSTRAINT `tblusersupplies_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tblusers` (`user_id`),
   CONSTRAINT `tblusersupplies_ibfk_2` FOREIGN KEY (`itemid`) REFERENCES `tblitems` (`itemid`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -202,7 +205,7 @@ CREATE TABLE `tblusersupplies` (
 
 LOCK TABLES `tblusersupplies` WRITE;
 /*!40000 ALTER TABLE `tblusersupplies` DISABLE KEYS */;
-INSERT INTO `tblusersupplies` VALUES (1,3,3,0,'MANUAL','2026-03-29 17:58:04'),(2,3,2,0,'MANUAL','2026-03-29 19:35:48'),(3,3,1,0,'MANUAL','2026-04-08 15:00:29'),(4,3,4,0,'MANUAL','2026-04-08 15:00:29');
+INSERT INTO `tblusersupplies` VALUES (1,3,3,0,'MANUAL','2026-03-29 17:58:04'),(2,3,2,0,'MANUAL','2026-03-29 19:35:48'),(3,3,1,1,'MANUAL','2026-04-08 15:00:29'),(4,3,4,1,'MANUAL','2026-04-08 15:00:29'),(5,1,1,1,'MANUAL','2026-04-10 21:00:22'),(6,1,10,1,'MANUAL','2026-04-10 21:00:27'),(7,1,7,1,'MANUAL','2026-04-10 21:00:28'),(8,1,5,1,'MANUAL','2026-04-10 21:00:29'),(9,1,8,1,'MANUAL','2026-04-10 21:00:30'),(10,9,9,1,'MANUAL','2026-04-10 21:04:24'),(11,9,11,1,'MANUAL','2026-04-10 21:04:24'),(12,9,2,1,'MANUAL','2026-04-10 21:04:25'),(13,9,10,1,'MANUAL','2026-04-10 21:04:25'),(14,9,4,1,'MANUAL','2026-04-10 21:04:26'),(15,9,3,1,'MANUAL','2026-04-10 21:04:30'),(16,9,1,1,'MANUAL','2026-04-10 21:04:31'),(17,9,6,1,'MANUAL','2026-04-10 21:04:31'),(18,9,7,1,'MANUAL','2026-04-10 21:04:32'),(19,9,5,1,'MANUAL','2026-04-10 21:04:32'),(20,9,8,1,'MANUAL','2026-04-10 21:04:32'),(21,7,3,1,'MANUAL','2026-04-10 21:06:39'),(22,7,8,1,'MANUAL','2026-04-10 21:07:04'),(23,7,10,1,'MANUAL','2026-04-10 21:07:04'),(24,7,2,1,'MANUAL','2026-04-10 21:07:05'),(25,3,11,1,'MANUAL','2026-04-10 21:08:10'),(26,3,6,1,'MANUAL','2026-04-10 21:08:10'),(27,3,7,1,'MANUAL','2026-04-10 21:08:10'),(28,3,5,1,'MANUAL','2026-04-10 21:08:11');
 /*!40000 ALTER TABLE `tblusersupplies` ENABLE KEYS */;
 UNLOCK TABLES;
 SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
@@ -216,4 +219,4 @@ SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-04-10 10:47:27
+-- Dump completed on 2026-04-10 17:15:31
